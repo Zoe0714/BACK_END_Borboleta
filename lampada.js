14dedfba726cebd277550f1627b4258eb2eff64f
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-02-27 11:52:48 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,26 @@
+const lamp = document.getElementById("lamp");
+const turnOn = document.getElementById("turnOn");
+const turnOff = document.getElementById("turnOff");
+
+function isLampBroken() {
+  return lamp.src.indexOf("quebrada") > -1;
+}
+
+function LampOn() {
+  if (!isLampBroken()) {
+    lamp.src = "ligada.jpg";
+  }
+}
+
+function LampOff() {
+  if (!isLampBroken()) {
+    lamp.src = "desligada.png";
+  }
+}
+
+function LampBroken() {
+  lamp.src = "quebrada.jpg";
+}
+turnOn.addEventListener("click", LampOn);
+turnOff.addEventListener("click", LampOff);
+lamp.addEventListener("dblclick", LampBroken);
